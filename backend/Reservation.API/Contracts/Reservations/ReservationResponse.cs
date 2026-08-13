d8ea9586325e242c3387ea6e7270935bf25c188a
zhizheng33782@gmail.com <zhizheng33782@gmail.com>
2026-08-12 23:09:35 -0400

    fix: remove unnecessary line breaks in ReservationResponse record

--- backend/Reservation.API/Contracts/Reservations/ReservationResponse.cs
+++ backend/Reservation.API/Contracts/Reservations/ReservationResponse.cs
@@ -2,30 +2,19 @@ using System.Text.Json.Serialization;
 
 namespace Reservation.API.Contracts.Reservations;
 
-
 public sealed record ReservationResponse(
-
     [property: JsonPropertyName("reservation_id")] int ReservationId,
-
     [property: JsonPropertyName("first_name")]
     string FirstName,
-
     [property: JsonPropertyName("last_name")]
     string LastName,
-
     [property: JsonPropertyName("email")]
     string Email,
-
     [property: JsonPropertyName("mobile_number")]
     string PhoneNumber,
-
     [property: JsonPropertyName("reservation_date")]
     DateOnly ReservationDate,
-
     [property: JsonPropertyName("reservation_time")]
     TimeOnly ReservationTime,
-
     [property: JsonPropertyName("people_count")]
-    int PeopleCount
-    
-);
+    int PeopleCount);
